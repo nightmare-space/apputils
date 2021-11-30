88a0d5f99edc168d77d7d430a5948f989c176e82
Nightmare <mengyanshou@gmail.com>
2021-11-30 14:42:47 +0800

    update applib

--- android/src/main/java/com/nightmare/apputils/ApputilsPlugin.java
+++ android/src/main/java/com/nightmare/apputils/ApputilsPlugin.java
@@ -14,6 +14,7 @@ import io.flutter.plugin.common.MethodChannel.Result;
 /**
  * ApputilsPlugin
  */
+
 public class ApputilsPlugin implements FlutterPlugin, MethodCallHandler {
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
